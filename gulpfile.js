26b79f1ed6a68a471e4b8b079c278717cdb0d7d7
Josney <josneysoares9@gmail.com>
2020-04-28 13:37:42 +0100

    'adicionei'

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 //'use strict'
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
 
 //task Sass
 gulp.task('sass', function(){
-	return sass('sass/*.sass').pipe(gulp.dest('css'))
+	return gulp.src('sass/**/*.sass')
+	.pipe(sass({outputStyle: 'compressed'})
+		.on('error', sass.logError))
+	.pipe(gulp.dest('css'))
 });
 
 //task Watch
 gulp.task('watch', function(){
-	gulp.watch('sass/*.sass', ['sass']);
+	gulp.watch('sass/**/*.sass', ['sass']);
 });
 
 //task default Gulp
